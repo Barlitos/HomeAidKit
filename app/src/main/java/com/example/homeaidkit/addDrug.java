621535409c08be937a35c73b017a9e5718171187
Kubon <b3myshade@gmail.com>
2020-09-17 12:09:49 +0200

    chyba wszystkie widoki

---
+++ app/src/main/java/com/example/homeaidkit/addDrug.java
@@ -0,0 +1,14 @@
+package com.example.homeaidkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addDrug extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_drug);
+    }
+}
---
+++ app/src/main/java/com/example/homeaidkit/editDrug.java
@@ -0,0 +1,14 @@
+package com.example.homeaidkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editDrug extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_drug);
+    }
+}
