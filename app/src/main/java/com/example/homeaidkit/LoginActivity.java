3b0859d375e80ecea0c6328b771f751c16e72adb
Barlitos <karolb156@gmail.com>
2020-10-15 00:04:34 +0200

    registration and login requests, getting response

--- app/src/main/java/com/example/homeaidkit/LoginActivity.java
+++ app/src/main/java/com/example/homeaidkit/LoginActivity.java
@@ -1,24 +1,49 @@
 package com.example.homeaidkit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String postUrl="http://192.168.8.118/HomeAidKit/login.php";
+    protected EditText login;
+    protected EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        login=findViewById(R.id.emailInput);
+        password=findViewById(R.id.passwordInput);
+
         Button signIn = findViewById(R.id.signInButton);
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openMainActivity();
+                PostRequest loginRequest=new PostRequest();
+                loginRequest.execute("login",login.getText().toString(),"password",password.getText().toString());
+                //openMainActivity();
             }
         });
 
@@ -31,6 +56,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+
     public void openMainActivity()
     {
         Intent intent = new Intent(this, MainActivity.class);
@@ -42,4 +68,73 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, register.class);
         startActivity(intent);
     }
+
+    // --- Async task - performing post call to server-----
+    private class PostRequest extends AsyncTask<String,Void ,String > {
+
+        @Override
+        protected void onPreExecute() {
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            // alert dialog ---DEBUGGING---- should delete
+
+           // AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
+            //alertDialog.setTitle("Alert");
+            //alertDialog.setMessage(s);
+            try {
+                JSONObject object=new JSONObject(s);
+              //  alertDialog.setMessage(object.getString("message"));
+                if(object.has("password") && object.getInt("password")==0)
+                {
+                    password.setError(object.getString("message"));
+                }
+                if(object.getInt("login")==0)
+                {
+                    login.setError(object.getString("message"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+              //      new DialogInterface.OnClickListener() {
+                //        public void onClick(DialogInterface dialog, int which) {
+                  //          dialog.dismiss();
+                    //    }
+                    //});
+            //alertDialog.show();
+            //------//
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings){
+
+            OkHttpClient client =new OkHttpClient();
+            RequestBody form=new FormBody.Builder()
+                    .add(strings[0],strings[1])
+                    .add(strings[2],strings[3])
+                    .build();
+            Request request=new Request.Builder()
+                    .url(postUrl)
+                    .post(form)
+                    .build();
+            Response response = null;
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert response != null;
+            try {
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "Unknown Error";
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/homeaidkit/register.java
+++ app/src/main/java/com/example/homeaidkit/register.java
@@ -1,24 +1,77 @@
 package com.example.homeaidkit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
+import android.view.textclassifier.TextLanguage;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import okhttp3.Call;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okio.BufferedSink;
 
 public class register extends AppCompatActivity {
+ protected EditText login;
+ protected EditText email;
+ protected EditText password;
+
+ private static final String postUrl="http://192.168.8.118/HomeAidKit/createAccount.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        login=findViewById(R.id.loginInput);
+        email=findViewById(R.id.emailInput);
+        password=findViewById(R.id.password1Input);
 
+        //Button with onclick listener
         Button register = findViewById(R.id.registerButton);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openRegisterActivity();
+                PostRequest registerRequest=new PostRequest();
+               registerRequest.execute("login",login.getText().toString(),"email",email.getText().toString(),"password",password.getText().toString());
+                //openLogingActivity();
             }
         });
 
@@ -31,15 +84,80 @@ public class register extends AppCompatActivity {
         });
     }
 
-    public void openRegisterActivity()
-    {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
-
     public void openLogingInActivity()
     {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+    // --- Async task - performing post call to server-----
+    private class PostRequest extends AsyncTask<String,Void ,String >{
+
+        @Override
+        protected void onPreExecute() {
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            // alert dialog ---DEBUGGING---- should delete
+
+            AlertDialog alertDialog = new AlertDialog.Builder(register.this).create();
+            alertDialog.setTitle("Alert");
+            //alertDialog.setMessage(s);
+            try {
+                JSONObject object=new JSONObject(s);
+                alertDialog.setMessage(object.getString("message"));
+                if(object.getInt("email")==0)
+                {
+                    email.setError(object.getString("message"));
+                }
+                if(object.getInt("login")==0)
+                {
+                    login.setError(object.getString("message"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+            //------//
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings){
+
+            OkHttpClient client =new OkHttpClient();
+            RequestBody form=new FormBody.Builder()
+                    .add(strings[0],strings[1])
+                    .add(strings[2],strings[3])
+                    .add(strings[4],strings[5])
+                    .build();
+            Request request=new Request.Builder()
+                    .url(postUrl)
+                    .post(form)
+                    .build();
+            Response response = null;
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert response != null;
+            try {
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "Unknown Error";
+        }
+
+    }
+
+
 }
